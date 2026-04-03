7471cb18c5095294b099fef7c3e47f9922f34eec
yasser <ya-1417@outlook.com>
2026-04-04 01:09:11 +0300

    final x3

--- src/components/PoemBlock.tsx
+++ src/components/PoemBlock.tsx
@@ -22,7 +22,7 @@ export function renderPoemVerses(text: string) {
 			className="glass p-5 flex-col shrink-0 h-auto gap-5 text-center"
 			initial={{opacity: 0, y: 20}}
 			animate={{opacity: 1, y: 0}}
-			transition={{duration: 0.8, delay: 2, ease: 'easeIn'}}
+			transition={{duration: 0.6, delay: 2}}
 		>
 			{verses.map(([sadr, ajz], index) => (
 				<motion.div
--- src/components/waitlist.tsx
+++ src/components/waitlist.tsx
@@ -80,7 +80,7 @@ export default function WaitlistPage() {
 						بشاير
 					</span>
 				</motion.h1>
-				<motion.div
+				{/* <motion.div
 					initial={{opacity: 0, y: 20}}
 					animate={{opacity: 1, y: 0}}
 					transition={{duration: 0.6, delay: 0.4, type: 'spring'}}
@@ -119,7 +119,7 @@ export default function WaitlistPage() {
 							</motion.div>
 						</>
 					))}
-				</motion.div>
+				</motion.div> */}
 
 				<div className="mt-8">
 					{/* <PoemBlock text={currentPoem} className="" /> */}
